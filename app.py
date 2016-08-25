28a288f7c1c4fc6fc402df5ed9a5b3f8490bef4f
bluetokey <bluetokey@yahoo.com>
2016-08-24 18:22:37 -0700

    minor bug fixes

--- app.py
+++ app.py
@@ -11,7 +11,7 @@
 '''
 __author__ = 'mytokarz'
 
-from flask import Flask, Response, send_from_directory
+from flask import Flask, Response, send_from_directory, redirect,request
 from dcjeevestree import dcjeevestree
 import os
 import re
@@ -20,7 +20,6 @@ import re
 app_dir = "./"
 utterance_file = app_dir + "utterance.conf"
 tree = dcjeevestree()
-
 
 # Read in the utterance config file
 def readinconfigs():
@@ -44,18 +43,35 @@ def print_json():
     '''
     return  tree.getjson(), 200, {'content_type': 'application/json'}
 
+@app.route('/validate')
+def validate_sentence():
+    '''
+    Validate an utterance is supported.
+    '''
+    if request.args.get("sentence") and tree.includes(request.args.get("sentence")):
+        return "looks good!", 200, {'content_type': 'text/html'}
+    else:
+        return "I don't understand this, try <A href='/help'>/help", 200, {'content_type': 'text/html'}
+
+@app.route('/go')
+def dcjeeves_go():
+    '''
+    Return JSON version of utterance file.
+    '''
+    return  tree.getjson(), 200, {'content_type': 'application/json'}
+
 
 @app.route('/help')
 def print_help():
     '''
     Build a help response.
     '''
-    return send_from_directory(os.path.join(app_dir,'static'),'app-help.txt'), 200, {'content_type': 'text'}
+    return send_from_directory(os.path.join(app_dir,'static'),'app-help.html'), 200, {'content_type': 'text/html'}
 
 
 @app.route('/')
 def hello_world():
-    return 'DCJeeves landing page, try /help'
+    return redirect("/help", code=302)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
